579d414849ecf2809b7c01d79195e00b25bed595
YanaB-b <j.biliuk96@gmail.com>
2023-04-25 08:34:31 +0200

    зміни

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,17 +4,17 @@ import Filter from '../Filter/Filter';
 import ContactsList from '../ContactsList/ContactsList';
 import css from './App.module.css';
 
-export const App =()=>{
+export const App = () => {
   const [contacts, setContacts] = useState(
- ()=> 
-   [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    )
-    const [filter, setFilter] = useState('');
+    () =>
+      JSON.parse(localStorage.getItem('contacts')) || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+  const [filter, setFilter] = useState('');
 
   // componentDidMount() {
   //   const contacts = localStorage.getItem('contacts');
@@ -33,10 +33,10 @@ export const App =()=>{
   useEffect(() => {
     contacts.length &&
       localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts])
+  }, [contacts]);
 
- const addContacts = newContact => {
-    const duplicate =contacts.find(
+  const addContacts = newContact => {
+    const duplicate = contacts.find(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
 
@@ -44,45 +44,36 @@ export const App =()=>{
       alert(`${newContact.name} is already in contacts`);
       return;
     }
-    setContacts(prevContacts => 
-       [newContact, ...prevContacts],
-    );
+    setContacts(prevContacts => [newContact, ...prevContacts]);
   };
 
   // Оневлення фільтра
   const changeFilter = e => {
-   setFilter(  e.currentTarget.value );
+    setFilter(e.currentTarget.value);
   };
 
   // Нова колекція
   const getVisibleContacts = () => {
-   
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
   const onDelete = contactId => {
-    setContacts(prevContacts => 
-      prevContacts.filter(contact => contact.id !== contactId),
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
   };
- 
-    return (
-      <div className={css.phonebook}>
-        <h1>Phonebook</h1>
-        <ContactForm
-          onContacts={addContacts}
-          contacts={contacts}
-        />
 
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={changeFilter} />
-        <ContactsList
-          contacts={getVisibleContacts()}
-          onDelete={onDelete}
-        />
-      </div>
-    );
-    }
+  return (
+    <div className={css.phonebook}>
+      <h1>Phonebook</h1>
+      <ContactForm onContacts={addContacts} contacts={contacts} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+      <ContactsList contacts={getVisibleContacts()} onDelete={onDelete} />
+    </div>
+  );
+};
 export default App;
